b7ff2ee2e84cc7c98763dcff53d470ef9132c2d0
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2022-10-06 18:41:08 +0100

    issues with text fix

--- todolist/App.js
+++ todolist/App.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { StyleSheet, Text, View, TextInput, KeyboardAvoidingView, TouchableOpacity, Platform } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
 import Task from "./components/Task";
 
 export default function App() {
@@ -9,29 +17,29 @@ export default function App() {
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>Today's Tasks</Text>
 
-        <View style={styles.items}>{/* This is where tasks will go */}</View>
-          <Task text={'Task 1'}/>
-          <Task text={'Task 2'}/>
+        <View style={styles.items}>
+          {/* This is where tasks will go */}
+
+          <Task text={"Task 1"} />
+          <Task text={"Task 2"} />
+        </View>
       </View>
+      // Write a task
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'Write a task'} />
+
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
-
-// Write a task
-
-<KeyboardAvoidingView
-  behavior={Platform.OS === "ios" ? "padding" : "height"}
-  style={styles.writeTaskWrapper}
->
-<TextInput />
-
-<TouchableOpacity>
-  <View style={styles.addWrapper}>
-    <Text style={styles.addText}>+</Text>
-  </View>
-</TouchableOpacity>
-
-</KeyboardAvoidingView>
 
 const styles = StyleSheet.create({
   container: {
@@ -44,9 +52,17 @@ const styles = StyleSheet.create({
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   items: {
-    marginTop: 30
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
 });
